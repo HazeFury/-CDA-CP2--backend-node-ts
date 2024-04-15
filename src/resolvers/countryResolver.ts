4dc55a22fc1e52eeac12f7470256c534ceb57204
Marc-Antoine B <115265331+HazeFury@users.noreply.github.com>
2024-04-15 12:11:00 +0200

    queries to search all country and country by ID works well

--- src/resolvers/countryResolver.ts
+++ src/resolvers/countryResolver.ts
@@ -1,11 +1,21 @@
 import { Resolver, Query, Mutation, Arg } from "type-graphql";
 import { Country, NewCountryInput } from "./../entities/country";
+import { GraphQLError } from "graphql";
 
 @Resolver(Country)
 export default class AdsResolver {
   @Query(() => [Country])
   async countries() {
     return Country.find();
+  }
+
+  @Query(() => Country)
+  async getCountryByCode(@Arg("countryCode") code: string) {
+    const country = await Country.findOne({
+      where: { code },
+    });
+    if (!country) throw new GraphQLError("not found");
+    return country;
   }
 
   @Mutation(() => Country)
